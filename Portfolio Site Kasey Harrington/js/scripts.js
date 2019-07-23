740664063aebf6e8c7cd69d5ca2ccec12896e89d
Kasey <kaseyaharrington@gmail.com>
2019-07-23 14:33:05 -0700

    jQuery updates

--- Portfolio Site Kasey Harrington/js/scripts.js
+++ Portfolio Site Kasey Harrington/js/scripts.js
@@ -1,1 +1,8 @@
 console.log("Hi! Welcome to my Portfolio Site")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
